dfc035186e3e696c38c6359b9b25b31bfe0c81f6
Muhammad Talha <talhariaz5425869@gmail.com>
2023-07-05 08:14:04 +0000

    fd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 app.use('/api/v1',(req,res)=>{
-  res.json({hi:'his this is text'})
+  res.json({hi:'his this s text'})
 })
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
