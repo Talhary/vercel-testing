75e695213f31b862c11ed37a4f06a5e5acb6f7ff
Muhammad Talha <talhariaz5425869@gmail.com>
2023-07-05 08:19:59 +0000

    Pending changes exported from your codespace

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+app.use('/api/v1',(req,res)=>{
+  res.json({hi:'his this s text'})
+})
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
+});
