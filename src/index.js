1a481792697367a517c6ef5569a3cd9066f6e572
Muhammad Talha <talhariaz5425869@gmail.com>
2023-07-05 08:08:05 +0000

    updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const app = require('./app');
-
+app.use('/api/v1',(req,res)=>{
+  res.json({hi:'his this is text'})
+})
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   /* eslint-disable no-console */
